7dc2487c2bb984135f5abce3f36afffb328c1d1c
AlexWang <tsanghaiwang@gmail.com>
2025-08-27 14:04:32 +1000

    updated the prompts

--- src/multi-agent/furhat_client.py
+++ src/multi-agent/furhat_client.py
@@ -1,16 +1,8 @@
 from furhat_remote_api import FurhatRemoteAPI
 
+furhat = FurhatRemoteAPI("localhost")
 
-class Client:
-    
-    def __init__(self):
-        self.furhat = FurhatRemoteAPI("localhost")
-        self.furhat.set_voice(name="Matthew")
-        pass
-    def start(self):
-        self.furhat.say(text="Hello from the Virtual Lab!")
-        self.furhat.gesture(name="BrowRaise")
-
-    
+def getFurhat():
+    return furhat    
 
 
--- src/multi-agent/main.py
+++ src/multi-agent/main.py
@@ -1,29 +1,29 @@
-from openai_client import agents, ask_gpt
-from furhat_remote_api import FurhatRemoteAPI
+from openai_client import *
+from furhat_client import *
 
 def main():
-    furhat = FurhatRemoteAPI("localhost")
-
+    furhat = getFurhat()
     voices = furhat.get_voices()
     furhat.set_voice(name="Matthew")
 
     furhat.say(text="Hello!")
     furhat.gesture(name="BrowRaise")
-
+    print(furhat.get_gestures)
     #agent_manager = agents()
 
     while True:
+        print("Connected, start listening")
         result = furhat.listen()
-        user_text = result.message
-        if (user_text == "" ):
+        if (result.message == "" or result.success == False):
             continue
-        print(f"User said: {user_text}")
+        user_text = result.message
+        print(result)
 
-        gpt_response = ask_gpt(str(user_text))
-        print(f'Responces: {gpt_response}')
-        furhat.say(text=gpt_response)
-
-
+        messages = []
+        messages.append({"role": "user", "content": user_text})
+        new_messages = run_full_turn(agents[0], messages)
+        print(f'Responces: {new_messages}')
+        furhat.say(text=new_messages.messages[0].content)
 
 if __name__ == "__main__":
     main()
